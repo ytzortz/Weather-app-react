bb55f27e5dcfc25524a8c1907ff8628dacbce067
Ioannis Tzortzakis <ioannis.tzortzak@gmail.com>
2024-06-14 23:36:56 +0300

    fixed some issues. Added big title comp

---
+++ project/src/components/BigTitle.js
@@ -0,0 +1,7 @@
+const BigTitle = ({ title }) => {
+  return (
+    <h1 style={{ textAlign: 'center', marginBottom: '50%' }}>{title}</h1>
+  );
+};
+
+export default BigTitle;
---
+++ project/src/pages/Home.js
@@ -0,0 +1,13 @@
+import BigTitle from '../components/BigTitle';
+
+
+const Home = () => {
+    return (
+      <div>
+        <BigTitle title="Choose a (something related to API)"/>
+      </div>
+    );
+  };
+  
+  export default Home;
+  
